ae84d2237998a4b04d6d2f31fe9a289b9d19bc26
Francisco Ernesto Teixeira <fco.ernesto@gmail.com>
2026-04-21 19:14:18 -0300

    Updated models with Admin API

--- app/Models/Edition.php
+++ app/Models/Edition.php
@@ -3,28 +3,44 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Throwable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Edition extends Model
 {
+    use SoftDeletes;
+
+    const DELETED_AT = 'removed_at';
 
     protected $fillable = [
-        'year', 'options', 'active', 'created_at', 'updated_at', 'removed_at'
+        'year',
+        'options',
+        'active',
+        'removed_at',
     ];
 
     protected $casts = [
-        'options' => 'array',
-        'active' => 'boolean',
+        'options'    => 'array',
+        'active'     => 'boolean',
+        'removed_at' => 'datetime',
     ];
 
-    public function getOptionsAttribute($value)
+    public function talks()
     {
-        try {
-            $decoded = json_decode($value);
-            return json_last_error() === JSON_ERROR_NONE ? $decoded : null;
-        } catch (Throwable $e) {
-            return null;
-        }
+        return $this->hasMany(Talk::class, 'edition_id');
     }
 
+    public function participants()
+    {
+        return $this->hasMany(Participant::class, 'edition_id');
+    }
+
+    public function collaborators()
+    {
+        return $this->hasMany(Collaborator::class, 'edition_id');
+    }
+
+    public function organizers()
+    {
+        return $this->hasMany(Organizer::class, 'edition_id');
+    }
 }
